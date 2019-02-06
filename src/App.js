a0581fc9408b548279251b7afc0a36d40e76d4d0
alu@Zed-hp <farhim.ferdous3@gmail.com>
2019-02-06 20:17:14 +0600

    resetting and resuming work on linux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import RouterTest from "./components/RouterTest";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AddButton from "./components/AddButton";
 import TodoList from "./components/todo/TodoList";
 import AddListForm from "./components/todo/AddListForm";
-import RouterTest from "./components/RouterTest";
 
 class App extends Component {
   state = {
